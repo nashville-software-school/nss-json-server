8868ddcb457bb6f8240d3a638e39cbb23b810c3c
Steve Brownlee <chortlehoort@gmail.com>
2019-12-01 09:47:21 -0600

    First commit with updated docs

--- src/bin.ts
+++ src/bin.ts
@@ -13,66 +13,67 @@ import run = require('json-server/lib/cli/run')
 // https://github.com/typicode/json-server/blob/master/src/cli/index.js
 
 const argv = yargs
-	.config('config')
-	.usage('$0 [options] <source>')
-	.options({
-		port: { alias: 'p', description: 'Set port', default: 3000 },
-		host: { alias: 'H', description: 'Set host', default: 'localhost' },
-		watch: { alias: 'w', description: 'Watch file(s)' },
-		routes: { alias: 'r', description: 'Path to routes file' },
-		middlewares: { alias: 'm', array: true, description: 'Paths to middleware files' },
-		static: { alias: 's', description: 'Set static files directory' },
-		'read-only': { alias: 'ro', description: 'Allow only GET requests' },
-		'no-cors': { alias: 'nc', description: 'Disable Cross-Origin Resource Sharing' },
-		'no-gzip': { alias: 'ng', description: 'Disable GZIP Content-Encoding' },
-		snapshots: { alias: 'S', description: 'Set snapshots directory', default: '.' },
-		delay: { alias: 'd', description: 'Add delay to responses (ms)' },
-		id: { alias: 'i', description: 'Set database id property (e.g. _id)', default: 'id' },
-		foreignKeySuffix: {
-			alias: 'fks',
-			description: 'Set foreign key suffix (e.g. _id as in post_id)',
-			default: 'Id',
-		},
-		quiet: { alias: 'q', description: 'Suppress log messages from output' },
-		config: { alias: 'c', description: 'Path to config file', default: 'json-server.json' },
-	})
-	.boolean('watch')
-	.boolean('read-only')
-	.boolean('quiet')
-	.boolean('no-cors')
-	.boolean('no-gzip')
-	.string('routes')
-	.help('help')
-	.alias('help', 'h')
-	.version(jsonServerPkg.version)
-	.alias('version', 'v')
-	.example('$0 db.json', '')
-	.example('$0 file.js', '')
-	.example('$0 http://example.com/db.json', '')
-	.epilog(
-		'https://github.com/typicode/json-server\nhttps://github.com/jeremyben/json-server-auth'
-	)
-	.require(1, 'Missing <source> argument').argv
+    .config('config')
+    .usage('$0 [options] <source>')
+    .options({
+        port: { alias: 'p', description: 'Set port', default: 3000 },
+        host: { alias: 'H', description: 'Set host', default: 'localhost' },
+        watch: { alias: 'w', description: 'Watch file(s)' },
+        routes: { alias: 'r', description: 'Path to routes file' },
+        expires: { alias: 'X', description: 'Token expiration time (e.g. 7h)' },
+        middlewares: { alias: 'm', array: true, description: 'Paths to middleware files' },
+        static: { alias: 's', description: 'Set static files directory' },
+        'read-only': { alias: 'ro', description: 'Allow only GET requests' },
+        'no-cors': { alias: 'nc', description: 'Disable Cross-Origin Resource Sharing' },
+        'no-gzip': { alias: 'ng', description: 'Disable GZIP Content-Encoding' },
+        snapshots: { alias: 'S', description: 'Set snapshots directory', default: '.' },
+        delay: { alias: 'd', description: 'Add delay to responses (ms)' },
+        id: { alias: 'i', description: 'Set database id property (e.g. _id)', default: 'id' },
+        foreignKeySuffix: {
+            alias: 'fks',
+            description: 'Set foreign key suffix (e.g. _id as in post_id)',
+            default: 'Id',
+        },
+        quiet: { alias: 'q', description: 'Suppress log messages from output' },
+        config: { alias: 'c', description: 'Path to config file', default: 'json-server.json' },
+    })
+    .boolean('watch')
+    .boolean('read-only')
+    .boolean('quiet')
+    .boolean('no-cors')
+    .boolean('no-gzip')
+    .string('routes')
+    .help('help')
+    .alias('help', 'h')
+    .version(jsonServerPkg.version)
+    .alias('version', 'v')
+    .example('$0 db.json', '')
+    .example('$0 file.js', '')
+    .example('$0 http://example.com/db.json', '')
+    .epilog(
+        'https://github.com/typicode/json-server\nhttps://github.com/jeremyben/json-server-auth'
+    )
+    .require(1, 'Missing <source> argument').argv
 
 // Add our index path to json-server middlewares.
 if (argv.middlewares) {
-	;(<string[]>argv.middlewares).unshift(__dirname)
+    ; (<string[]>argv.middlewares).unshift(__dirname)
 } else {
-	argv.middlewares = [__dirname]
+    argv.middlewares = [__dirname]
 }
 
 // Adds guards to json-server routes.
 // We are forced to create an intermediary file:
 // https://github.com/typicode/json-server/blob/master/src/cli/run.js#L109
 if (argv.routes) {
-	let routes = JSON.parse(readFileSync(argv.routes, 'utf8'))
-	routes = parseGuardsRules(routes)
-	routes = JSON.stringify(routes)
+    let routes = JSON.parse(readFileSync(argv.routes, 'utf8'))
+    routes = parseGuardsRules(routes)
+    routes = JSON.stringify(routes)
 
-	const tmpFilepath = join(tmpdir(), `routes-from-${basename(process.cwd())}.json`)
-	writeFileSync(tmpFilepath, routes, 'utf8')
+    const tmpFilepath = join(tmpdir(), `routes-from-${basename(process.cwd())}.json`)
+    writeFileSync(tmpFilepath, routes, 'utf8')
 
-	argv.routes = tmpFilepath
+    argv.routes = tmpFilepath
 }
 // But we won't be able to properly watch and reload custom routes:
 // https://github.com/typicode/json-server/blob/master/src/cli/run.js#L229
--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,15 @@
+import * as yargs from 'yargs'
+
+const expires = yargs.argv.X
+
 export const JWT_SECRET_KEY = 'json-server-auth-123456'
 
-export const JWT_EXPIRES_IN = '1h'
+export const JWT_EXPIRES_IN = <string>expires || '1h'
 
 export const SALT_LENGTH = 10
 
 export const EMAIL_REGEX = new RegExp(
-	"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+    "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
 )
 
 export const MIN_PASSWORD_LENGTH = 4
